e8b29ca0355035efa2dffddf7b9eee85ecd3c2ba
shturkhal <107053994+shturkhal@users.noreply.github.com>
2024-03-20 11:29:08 +0200

    Initial Commit

---
+++ ImplementUnitTesting/ImplementUnitTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImplementUnitTestingApp.swift
+//  ImplementUnitTesting
+//
+//  Created by Іван Штурхаль on 20.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct ImplementUnitTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
